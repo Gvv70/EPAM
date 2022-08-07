d3b11d9a7eba305164ee82a8326b6212756fc47f
Gvv70 <104983204+Gvv70@users.noreply.github.com>
2022-08-07 16:36:29 +0300

    Delete Exception directory

--- Exception/src/com/company/CheckMail.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class CheckMail {
-    public boolean checkMail(String eMail){
-        return eMail.lastIndexOf('@') != -1;
-    }
-}
--- Exception/src/com/company/exception/UnderAgeException.java
+++
@@ -1,9 +0,0 @@
-package com.company.exception;
-
-public class UnderAgeException extends Throwable {
-    public static void underAgeException(int age){
-        if (age < 18){
-            System.err.println("Person's age is under 18 years");
-        }
-    }
-}
